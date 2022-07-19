efba08b37d1c17f75080847a0ff33b2fb960aabc
Bruno Guimarães <devgmrs@gmail.com>
2022-07-18 22:15:23 -0300

    Aula 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
-const formulario = document.querySelector('#novoItem');
+const formulario = document.querySelector('#formularioItens');
+const lista = document.querySelector('#lista');
+
+const limpaInput = (form) => {
+	form.children.nome.value = '';
+	form.children.quantidade.value = '';
+
+	form.children.nome.focus();
+};
+
+const criaElementoEmTela = (nome, quantidade) => {
+	const itemLista = document.createElement('li');
+	itemLista.classList.add('item');
+
+	const quantidadeItem = document.createElement('strong');
+	quantidadeItem.innerHTML = quantidade;
+
+	const nomeItem = document.createElement('p');
+	nomeItem.innerHTML = nome;
+
+	lista.appendChild(itemLista);
+
+	itemLista.appendChild(quantidadeItem);
+	itemLista.appendChild(nomeItem);
+};
 
 formulario.addEventListener('submit', (ev) => {
-	console.log(ev);
 	ev.preventDefault();
 
 	const nome = ev.target.elements.nome.value;
 	const quantidade = ev.target.elements.quantidade.value;
+
+	criaElementoEmTela(nome, quantidade);
+	limpaInput(formulario);
 });
